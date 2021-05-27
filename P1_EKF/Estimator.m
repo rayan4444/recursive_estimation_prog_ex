bc80a1dcf4e4d7b472c45ef1ff7bf74f73e34123
rayan4444 <rayan.armani@gmail.com>
2021-05-27 12:40:08 +0200

    Add matrices A and L
    Someone should double check my partial derivatives for matrix A because they are huge

--- P1_EKF/Estimator.m
+++ P1_EKF/Estimator.m
@@ -91,7 +91,7 @@ if (tm == 0)
     % estimator variance init (initial posterior variance)
     estState.Pm = diag([posVar linVelVar oriVar windVar driftVar]); % diagonal matrix 7x7
     % estimator state
-    estState.xm = [posEst linVelEst oriEst windEst driftEst]; % 1x7 matrix
+    estState.xm = [posEst'; linVelEst'; oriEst; windEst; driftEst]; % 7x1 matrix
     % time of last update
     estState.tm = tm;
 end
@@ -101,9 +101,38 @@ end
 dt = tm - estState.tm;
 estState.tm = tm; % update measurement update time
 
-% prior update
+% Easier to write variables
+% constants
+C_dh = estConst.dragCoefficientHydr;
+C_da = estConst.dragCoefficientAir;
+C_r = estConst.rudderCoefficient; 
+C_w = estConst.windVel;
 
-% measurement update
+% Process noise variance
+Q_d = estConst.DragNoise ;
+Q_r = estConst.RudderNoise ;
+Q_rho= estConst.WindAngleNoise ;
+Q_b = estConst.GyroDriftNoise ;
+
+% Measurement noise variance
+s_a2 = estConst.DistNoiseA;
+s_b2= estConst.DistNoiseB;
+s_c2= estConst.DistNoiseC;
+s_g2= const.GyroNoise;
+s_n2 = const.CompassNoise;
+
+
+
+
+%% prior update
+
+% Prepare useful matrices
+Qc=diag([estConst.DragNoise estconst.RudderNoise estConst.GyroDriftNoise])
+
+
+
+
+%% measurement update
 
 % Get resulting estimates and variances
 % Output quantities
@@ -119,4 +148,90 @@ oriVar = ...
 windVar = ...
 driftVar = ...
 
+end
+
+
+% Function for system dynamics
+function [q]= get_q(x_hat,estConst,u)
+    % input: xhat vector: px, py, sx, sx, phi, rho, b
+    
+    C_dh = estConst.dragCoefficientHydr;
+    C_da = estConst.dragCoefficientAir;
+    C_w = estConst.windVel;
+    C_r = estConst.rudderCoefficient; 
+    
+    % make it easier to read
+    sx = x_hat(3);
+    sy = x_hat(4);
+    phi = x_hat(5);
+    rho = x_hat(6);
+    
+    % returns a 7x1 vector
+    % x_dot = q(x_hat, 0, t)
+    % all noises = 0 beacuse they are zero mean 
+    q = zeros(7,1);
+    q(1)= sx; %sx
+    q(2)= sy; %sy
+    q(3)= cos(phi)*(tanh(u(1))- C_dh(sx^2+sy^2))-C_da(sx-Cw*cos(rho))*sqrt((sx-C_w*cos(rho))^2+(sy-C_w*sin(rho))^2);
+    q(4)= sin(phi)*(tanh(u(1))- C_dh(sx^2+sy^2))-C_da(sy-Cw*sin(rho))*sqrt((sx-C_w*cos(rho))^2+(sy-C_w*sin(rho))^2);
+    q(5)= C_r*u(2);
+    %q(6)= 0  and q(7) =0
+    
+end 
+
+% Functions for A nad L matrices
+
+function [A] = get_A(x_hat, estConst, u)
+    % A is 7x7 matrix: A = dq/dx
+    C_dh = estConst.dragCoefficientHydr;
+    C_da = estConst.dragCoefficientAir;
+    C_w = estConst.windVel;
+    C_r = estConst.rudderCoefficient; 
+    
+    % make it easier to read
+    sx = x_hat(3);
+    sy = x_hat(4);
+    phi = x_hat(5);
+    rho = x_hat(6);
+    
+    A = zeros(7); %7x7 zeros matrix
+    A(1,3)= 1; % dq(1)/d(sx)
+    A(2,4) = 1; % dq(2)/d(sx)
+    
+    % 13 = c_da; 7 = C_dh; A = tanh(ut)
+    % holy shit this is going to be one very long partial derivative to get
+    
+    A(3,3) = -(C_da*(sx - C_w*cos(rho))^2)/sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - C_da*sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - 2*C_dh*sx*cos(phi); %dq(3)/d(sx)
+    A(3,4) = -(C_da*(x - C_w*cos(rho))*(sy - C_w*sin(rho)))/sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - 2*C_dh*sy*cos(phi); %dq(3)/d(sy)
+    A(3,5) = -sin(phi)*(tanh(u(1)) - C_hd*(sx^2 + sy^2)); %dq(3)/d(phi)
+    A(3,6) = -C_da*C_w*sin(rho)*sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - (C_da*(sx - C_w*cos(rho))*(2*C_w*sin(rho)*(sx - C_w*cos(rho)) - 2*C_w*cos(rho)*(sy - C_w*sin(rho))))/(2*sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2)); %dq/d(rho)
+    
+    A(4,3) = -(C_da*(sx - C_w*cos(rho))*(sy - C_w*sin(rho)))/sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - 2*C_dh*sx*sin(phi);%dq4/d(sx)
+    A(4,4) = -(C_da*(sy - C_w*sin(rho))^2)/sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - C_da*sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - 2*C_dh*sy*sin(phi);%dq4/d(sy)
+    A(4,5) = cos(phi)*(tanh(u(1)) - C_da*(sx^2 + sy^2));
+    A(4,6) = C_da*C_w*cos(rho)*sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2) - (C_da*(sy - C_w*sin(rho))*(2*C_w*sin(rho)*(sx - C_w*cos(rho)) - 2*C*cos(rho)*(sy - C_w*sin(rho))))/(2*sqrt((sx - C_w*cos(rho))^2 + (sy - C_w*sin(rho))^2)) ; %dq/d(rho)
+end 
+
+function [L]= get_L((x_hat, estConst, u)
+    % process noise vector v= [vd; vr; vrho; vb]
+    
+    C_dh = estConst.dragCoefficientHydr;
+    C_da = estConst.dragCoefficientAir;
+    C_w = estConst.windVel;
+    C_r = estConst.rudderCoefficient; 
+    
+    % make it easier to read
+    sx = x_hat(3);
+    sy = x_hat(4);
+    phi = x_hat(5);
+    rho = x_hat(6);
+    
+    % So L = dq/dv is 7x4 matrix
+    L = zeros(7,4);
+    %L(1,:), L(2,:) = 0 position not related to noise
+    L(3,1) = -cos(phi)*C_dh*(sx^2+sy^2);
+    L(4,1) = -sin(phi)*C_dh*(sx^2+sy^2);
+    L(5,2)= C_r*u(2);
+    L(6,3)= 1;
+    L(7,4)= 1;
 end
